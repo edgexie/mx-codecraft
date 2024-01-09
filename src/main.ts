0eb4c401165e43a19a976ea88d677062e0543af5
edgex <edgexie@qq.com>
2024-01-09 16:56:54 +0800

    fix: cors add allowed headers

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors()
+  app.enableCors({
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 允许的HTTP方法
+    credentials: true, // 是否允许发送凭证（例如 cookies）
+    allowedHeaders: ['Content-Type'], // 允许的请求头
+  })
   await app.listen(3000)
 }
 bootstrap()
